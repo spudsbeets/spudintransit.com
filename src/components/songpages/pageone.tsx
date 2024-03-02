64f984643835702e0c79dd0ea3020bb689255c8a
Sean Miller <millersean757@gmail.com>
2024-03-02 13:11:45 -0700

    added some imgs

--- src/components/songpages/pageone.tsx
+++ src/components/songpages/pageone.tsx
@@ -11,14 +11,14 @@ const SongPageOne = () => {
                 <h3 className="release-title" id="c-flower-mountain-title">C Flower Mountain</h3>
                 </div></a>
                 <div id="tuesday-with-gp-div" className="release-divs">
-                <img className="release-image" id="tuesday-with-gp" alt="tuesday-with-gp" src="./src/images/five.png"></img>
+                <img className="release-image" id="tuesday-with-gp" alt="tuesday-with-gp" src="./src/images/tue-cover.png"></img>
                 <div className="release-explanation-div" id="tuesday-with-gp-explanation-div">
                 <p className="release-explanation" id="tuesday-with-gp-explanation">half of a larger story, tuesday with gunther and pharoah demonstrates an 'ordinary' day in the lives of my two cats in their comfy home</p>
                 </div>
                 <h3 className="release-title" id="tuesday-with-gp-title">Tuesday with Gunther and Pharoah</h3>
                 </div>                
                 <div id="wednesday-with-gp-div" className="release-divs">
-                <img className="release-image" id="wednesday-with-gp" alt="wednesday-with-gp" src="./src/images/four.png"></img>
+                <img className="release-image" id="wednesday-with-gp" alt="wednesday-with-gp" src="./src/images/wed-cover.png"></img>
                 <div className="release-explanation-div" id="wednesday-with-gp-explanation-div">
                 <p className="release-explanation" id="wednesday-with-gp-explanation">half of a larger story, wednesday with gunther and pharoah is the tale of a day that starts with catnip that launches a wild expedition of self-discovery for my girls</p>
                 </div>
