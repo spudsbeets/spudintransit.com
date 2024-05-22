18f531471e14bfdcc2c3a62691443f7632a771c1
Sean Miller <millersean757@gmail.com>
2024-05-22 08:15:15 -0600

    added album links

--- src/components/songpages/pageone.tsx
+++ src/components/songpages/pageone.tsx
@@ -10,20 +10,20 @@ const SongPageOne = () => {
                 </div>
                 <h3 className="release-title" id="c-flower-mountain-title">C Flower Mountain</h3>
                 </div></a>
-                <div id="tuesday-with-gp-div" className="release-divs">
+                <a href="https://open.spotify.com/album/3nDfz40mSeu1JFF44A8ZqQ?si=w5Wf-kvQTq2QawDo52guTg" target="_blank" className="links-anchors"><div id="tuesday-with-gp-div" className="release-divs">
                 <img className="release-image" id="tuesday-with-gp" alt="tuesday-with-gp" src="/images/tue-cover.png"></img>
                 <div className="release-explanation-div" id="tuesday-with-gp-explanation-div">
                 <p className="release-explanation" id="tuesday-with-gp-explanation">half of a larger story, tuesday with gunther and pharoah demonstrates an 'ordinary' day in the lives of my two cats in their comfy home</p>
                 </div>
                 <h3 className="release-title" id="tuesday-with-gp-title">Tuesday with Gunther and Pharoah</h3>
-                </div>                
-                <div id="wednesday-with-gp-div" className="release-divs">
+                </div></a>                
+                <a href="https://open.spotify.com/album/3rIbQZ076SLLuK2C1YeFq9?si=gaqklOT-TXyI4L0MKhYf2w" target="_blank" className="links-anchors"><div id="wednesday-with-gp-div" className="release-divs">
                 <img className="release-image" id="wednesday-with-gp" alt="wednesday-with-gp" src="/images/wed-cover.png"></img>
                 <div className="release-explanation-div" id="wednesday-with-gp-explanation-div">
                 <p className="release-explanation" id="wednesday-with-gp-explanation">half of a larger story, wednesday with gunther and pharoah is the tale of a day that starts with catnip that launches a wild expedition of self-discovery for my girls</p>
                 </div>
                 <h3 className="release-title" id="wednesday-with-gp-title">Wednesday with Gunther and Pharoah</h3>
-                </div>
+                </div></a>
             </div>
         </div>
     )
